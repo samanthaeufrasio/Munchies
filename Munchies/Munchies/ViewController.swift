8eeb2c1a8490ea2a83633c7c8bf1e50ff9b6e0b8
vinicius-lavor <viniciusdelavorcavalcante@gmail.com>
2022-11-21 11:57:29 -0300

    config: SwiftLint config

---
+++ Munchies/Munchies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Munchies
+//
+//  Created by Vinícius Cavalcante on 21/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
