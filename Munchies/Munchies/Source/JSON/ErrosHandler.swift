ab7c7cf49432dd47254b1b1d7eb4e4c2d401523e
souzagabriela <ellabaptist@outlook.com>
2022-11-29 08:21:26 -0300

    Consumo do JSON

---
+++ Munchies/Munchies/Source/JSON/ErrosHandler.swift
@@ -0,0 +1,14 @@
+//
+//  ErrosHandler.swift
+//  Munchies
+//
+//  Created by Gabriela Souza Batista on 25/11/22.
+//
+
+import Foundation
+
+enum ErrorsHandler: Error {
+    case invalidData
+    case invalidResquest
+    case requestError
+}
