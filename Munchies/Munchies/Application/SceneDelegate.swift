c7804eae72399200b9cd08f99cbc1725a56740a8
vinicius-lavor <viniciusdelavorcavalcante@gmail.com>
2022-12-07 09:02:52 -0300

    :bug: fix: Change view

--- Munchies/Munchies/Application/SceneDelegate.swift
+++ Munchies/Munchies/Application/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
       
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = UINavigationController(rootViewController: TableViewController())
+        window.rootViewController = UINavigationController(rootViewController: SearchBarController())
         window.makeKeyAndVisible()
         self.window = window
     }
